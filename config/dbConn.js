487f5519f8b97e812e9477987ccc0ef7e2117c52
7Star Madiino <7star.madiino@gmail.com>
2024-12-20 19:07:16 +0300

    add dot env package

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 const mysql = require('mysql2/promise');
 
 
 const connections = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'api_first',
-    password: '',
+    host: PROCESS.env.HOST_NAME,
+    user: PROCESS.env.USER_NAME,
+    database: PROCESS.env.DB_NAME,
+    password: PROCESS.env.PASSWORD,
     dateStrings: true
 });
 
